3cb4b19c2653f8a5928f1dbdd2123f77a0c9f1e6
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-12 12:41:36 +0530

    Linked math.js, isEven.js, array-test.js.Array test cases working

--- src/arrays.js
+++ src/arrays.js
@@ -1,20 +1,23 @@
+const math = require("./math.js")
+const isEven = math.isEven;
 const selectEvenNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isEven(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (isEven(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
 const selectOddNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isOdd(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (isOdd(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
+exports.selectEvenNumbers = selectEvenNumbers;
--- src/math.js
+++ src/math.js
@@ -1,23 +1,25 @@
 const add = function(a, b) {
-	return a + b;
+  return a + b;
 }
 
 const multiply = function(a, b) {
-	return a * b;
+  return a * b;
 }
 
 const subtract = function(a, b) {
-	return a - b;
+  return a - b;
 }
 
 const divide = function(a, b) {
-	return a % b;
+  return a % b;
 }
 
 const isEven = function(a) {
-	return a % 2 == 0;
+  return a % 2 == 0;
 }
 
 const isOdd = function(a) {
-	return !isEven(a);
+  return !isEven(a);
 }
+
+exports.isEven = isEven;
